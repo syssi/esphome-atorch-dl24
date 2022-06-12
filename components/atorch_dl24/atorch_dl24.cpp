ad581683a66f2c9de2554a4fa290ee80a4cfb9e5
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-06-12 09:09:37 +0200

    Flip decoders

--- components/atorch_dl24/atorch_dl24.cpp
+++ components/atorch_dl24/atorch_dl24.cpp
@@ -174,6 +174,66 @@ void AtorchDL24::decode_ac_and_dc_(const uint8_t *data, uint16_t length) {
 
   // Example responses
   //
+  // FF.55.01.02.00.00.20.00.4E.20.00.13.FA.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1A.3C.00.00.00.00.09
+  // FF.55.01.02.00.00.20.00.4E.1E.00.13.FA.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1B.3C.00.00.00.00.08
+  // FF.55.01.02.00.00.20.00.4E.21.00.13.FB.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1C.3C.00.00.00.00.15
+  // FF.55.01.02.00.00.20.00.4E.20.00.13.FC.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1D.3C.00.00.00.00.16
+  // FF.55.01.02.00.00.20.00.4E.1B.00.13.FC.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1E.3C.00.00.00.00.0A
+  // FF.55.01.02.00.00.20.00.4E.23.00.13.FD.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1F.3C.00.00.00.00.1C
+  //
+  // 0xFF 0x55:            Magic header
+  // 0x01:                 Message type           1: Report (32 byte), 2: Reply (4 byte), 11: Command (6 byte)
+  // 0x02:                 Device type            1: AC meter, 2: DC meter, 3: USB meter
+  // 0x00 0x00 0x20:       Voltage                32 * 0.1 = 3.2 V
+  float voltage = dl24_get_24bit(4) * 0.1f;
+  this->publish_state_(this->voltage_sensor_, voltage);
+
+  // 0x00 0x4E 0x23:       Current                20003 * 0.001 = 20.003 A
+  float current = dl24_get_24bit(7) * 0.001f;
+  this->publish_state_(this->current_sensor_, current);
+  this->publish_state_(this->power_sensor_, voltage * current);
+
+  // 0x00 0x13 0xFD:       Capacity in Ah        5117 * 0.01 = 51.17 Ah
+  this->publish_state_(this->capacity_sensor_, dl24_get_24bit(10) * 0.01f);
+
+  // 0x00 0x00 0x00 0x11:  Energy in Wh           17 * 10.0 = 170.0
+  this->publish_state_(this->energy_sensor_, dl24_get_32bit(13) * 10.0f);
+
+  // 0x00 0x00 0x00:       Price per kWh          value * 0.01
+  // 0x00 0x00:            Frequency              value * 0.1 Hz
+  // 0x00 0x00:            Power factor           value * 0.001 kW?
+
+  // 0x00 0x25:            Temperature            37 °C
+  this->publish_state_(this->temperature_sensor_, (float) dl24_get_16bit(24));
+
+  // 0x00 0x02:            Hour                   2 h
+  // 0x21:                 Minute                 33 min
+  // 0x1F:                 Second                 31 sec
+  ESP_LOGD(TAG, "  Timer: %02d:%02d:%02d", dl24_get_16bit(26), data[28], data[29]);
+  if (previous_value_ != 61)
+    this->publish_state_(this->running_sensor_, (float) (previous_value_ != data[29]));
+  previous_value_ = data[29];
+
+  // 0x3C:                 Dim backlight          60 seconds
+  this->publish_state_(this->dim_backlight_sensor_, (float) data[30]);
+
+  // 0x00 0x00 0x00 0x00:  Unknown
+  // 0x1C:                 Checksum
+}
+
+void AtorchDL24::decode_usb_(const uint8_t *data, uint16_t length) {
+  auto dl24_get_16bit = [&](size_t i) -> uint16_t {
+    return (uint16_t(data[i + 0]) << 8) | (uint16_t(data[i + 1]) << 0);
+  };
+  auto dl24_get_24bit = [&](size_t i) -> uint32_t {
+    return (uint32_t(data[i + 0]) << 16) | (uint32_t(data[i + 1]) << 8) | (uint32_t(data[i + 2]) << 0);
+  };
+  auto dl24_get_32bit = [&](size_t i) -> uint32_t {
+    return (uint32_t(dl24_get_16bit(i + 0)) << 16) | (uint32_t(dl24_get_16bit(i + 2)) << 0);
+  };
+
+  // Example responses
+  //
   // FF.55.01.03.00.01.F3.00.00.00.00.06.38.00.00.03.11.00.07.00.0A.00.00.00.12.2E.33.3C.00.00.00.00.00.00.00.4E
   // FF.55.01.03.00.01.FB.00.00.00.00.3C.C7.00.00.55.4E.00.07.00.07.00.00.00.47.2F.24.3C.00.00.00.00.00.00.00.CE
   // FF.55.01.03.00.01.CD.00.00.7F.00.3C.C8.00.00.55.4E.00.09.00.0A.00.00.00.47.30.0D.3C.00.00.00.00.00.00.00.8F
@@ -219,66 +279,6 @@ void AtorchDL24::decode_ac_and_dc_(const uint8_t *data, uint16_t length) {
 
   // 0x00 0x00 0x00 0x00 0x00 0x00: Unknown
   // 0x4E:                 Checksum
-}
-
-void AtorchDL24::decode_usb_(const uint8_t *data, uint16_t length) {
-  auto dl24_get_16bit = [&](size_t i) -> uint16_t {
-    return (uint16_t(data[i + 0]) << 8) | (uint16_t(data[i + 1]) << 0);
-  };
-  auto dl24_get_24bit = [&](size_t i) -> uint32_t {
-    return (uint32_t(data[i + 0]) << 16) | (uint32_t(data[i + 1]) << 8) | (uint32_t(data[i + 2]) << 0);
-  };
-  auto dl24_get_32bit = [&](size_t i) -> uint32_t {
-    return (uint32_t(dl24_get_16bit(i + 0)) << 16) | (uint32_t(dl24_get_16bit(i + 2)) << 0);
-  };
-
-  // Example responses
-  //
-  // FF.55.01.02.00.00.20.00.4E.20.00.13.FA.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1A.3C.00.00.00.00.09
-  // FF.55.01.02.00.00.20.00.4E.1E.00.13.FA.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1B.3C.00.00.00.00.08
-  // FF.55.01.02.00.00.20.00.4E.21.00.13.FB.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1C.3C.00.00.00.00.15
-  // FF.55.01.02.00.00.20.00.4E.20.00.13.FC.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1D.3C.00.00.00.00.16
-  // FF.55.01.02.00.00.20.00.4E.1B.00.13.FC.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1E.3C.00.00.00.00.0A
-  // FF.55.01.02.00.00.20.00.4E.23.00.13.FD.00.00.00.11.00.00.00.00.00.00.00.00.25.00.02.21.1F.3C.00.00.00.00.1C
-  //
-  // 0xFF 0x55:            Magic header
-  // 0x01:                 Message type           1: Report (32 byte), 2: Reply (4 byte), 11: Command (6 byte)
-  // 0x02:                 Device type            1: AC meter, 2: DC meter, 3: USB meter
-  // 0x00 0x00 0x20:       Voltage                32 * 0.1 = 3.2 V
-  float voltage = dl24_get_24bit(4) * 0.1f;
-  this->publish_state_(this->voltage_sensor_, voltage);
-
-  // 0x00 0x4E 0x23:       Current                20003 * 0.001 = 20.003 A
-  float current = dl24_get_24bit(7) * 0.001f;
-  this->publish_state_(this->current_sensor_, dl24_get_24bit(7) * 0.001f);
-  this->publish_state_(this->power_sensor_, voltage * current);
-
-  // 0x00 0x13 0xFD:       Capacity in Ah        5117 * 0.01 = 51.17 Ah
-  this->publish_state_(this->capacity_sensor_, dl24_get_24bit(10) * 0.01f);
-
-  // 0x00 0x00 0x00 0x11:  Energy in Wh           17 * 10.0 = 170.0
-  this->publish_state_(this->energy_sensor_, dl24_get_32bit(13) * 10.0f);
-
-  // 0x00 0x00 0x00:       Price per kWh          value * 0.01
-  // 0x00 0x00:            Frequency              value * 0.1 Hz
-  // 0x00 0x00:            Power factor           value * 0.001 kW?
-
-  // 0x00 0x25:            Temperature            37 °C
-  this->publish_state_(this->temperature_sensor_, (float) dl24_get_16bit(24));
-
-  // 0x00 0x02:            Hour                   2 h
-  // 0x21:                 Minute                 33 min
-  // 0x1F:                 Second                 31 sec
-  ESP_LOGD(TAG, "  Timer: %02d:%02d:%02d", dl24_get_16bit(26), data[28], data[29]);
-  if (previous_value_ != 61)
-    this->publish_state_(this->running_sensor_, (float) (previous_value_ != data[29]));
-  previous_value_ = data[29];
-
-  // 0x3C:                 Dim backlight          60 seconds
-  this->publish_state_(this->dim_backlight_sensor_, (float) data[30]);
-
-  // 0x00 0x00 0x00 0x00:  Unknown
-  // 0x1C:                 Checksum
 }
 
 void AtorchDL24::publish_state_(sensor::Sensor *sensor, float value) {
