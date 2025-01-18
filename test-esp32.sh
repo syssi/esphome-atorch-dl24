77bad8645b2df4fbec04a30d3ecbf215d58da6c4
Sebastian Muszynski <basti@linkt.de>
2025-01-18 13:46:56 +0100

    Add ESP32C6 compatibility (#90)

--- test-esp32.sh
+++ test-esp32.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-esphome -s external_components_source components -s dl24_mac_address 41:B8:12:0A:A8:37 ${1:-run} ${2:-esp32-usb-meter-example.yaml}
+if [[ $2 == tests/* ]]; then
+  C="../components"
+else
+  C="components"
+fi
+
+esphome -s external_components_source $C -s dl24_mac_address 41:B8:12:0A:A8:37 ${1:-run} ${2:-esp32-usb-meter-example.yaml}
