f7b63f5c80b762781455228782c6e76ecfaa6e56
Sebastian Muszynski <basti@linkt.de>
2025-07-05 16:05:04 +0200

    Fix

--- test-esp32.sh
+++ test-esp32.sh
@@ -6,4 +6,4 @@ else
   C="components"
 fi
 
-esphome -s external_components_source $C -s dl24_mac_address 41:B8:12:0A:A8:37 ${1:-run} ${2:-esp32-usb-meter-example.yaml}
+esphome -s external_components_source $C -s dl24_mac_address 41:B8:12:0A:A8:37 ${1:-run} ${2:-esp32-usb-meter-example-faker.yaml}
