00ed7dc49adf048980f48ee5d12f22e519a5ad84
Sebastian Muszynski <basti@linkt.de>
2026-05-08 11:21:07 +0200

    Use C++17 concatenated namespace syntax, update .clang-tidy

--- components/atorch_dl24/button/atorch_button.h
+++ components/atorch_dl24/button/atorch_button.h
@@ -4,8 +4,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/button/button.h"
 
-namespace esphome {
-namespace atorch_dl24 {
+namespace esphome::atorch_dl24 {
 
 class AtorchDL24;
 class AtorchButton : public button::Button, public Component {
@@ -22,5 +21,4 @@ class AtorchButton : public button::Button, public Component {
   uint8_t holding_register_;
 };
 
-}  // namespace atorch_dl24
-}  // namespace esphome
+}  // namespace esphome::atorch_dl24
